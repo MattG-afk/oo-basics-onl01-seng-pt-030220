112ec064010c68d72ba7abe512e66be2c9ce6909
Matt Gordon <bigmattsize21@gmail.com>
2020-03-20 02:28:16 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,7 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition
+  attr_accessor :brand :color, :size, :material, :condition
   attr_reader :shoe
   def initialize(shoe)
     @shoe= shoe
   end
-  def brand
-    @brand
-  end
-    
 end
